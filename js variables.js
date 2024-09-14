ae2215eee31a895b7f9f70ee87a5bcd280861c92
riakjames211 <riakjamesngong@gmail.com>
2024-09-14 09:44:13 +0300

    Add files via upload
    riak james ngong 
    week two assignment

---
+++ js variables.js
@@ -0,0 +1,18 @@
+// variables
+var my_name = "riak james ngong";
+
+// assign a variable
+height = 5.8;
+
+console.log(my_name);
+
+console.log(height);
+
+// let
+let area = 68.2
+
+let f_name;
+f_name = "riak"
+
+console.log(area);
+console.log(f_name);
---
+++ my_js_program.js
@@ -0,0 +1,7 @@
+// this is my first JS application
+
+console.log("hello world")
+
+console.log("B28916")
+console.log("RIAK JAMES NGONG")
+console.log("BSIT")
