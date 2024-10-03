6b09ec9a5035788c9e86b014a4677dd3c6cbab35
riakjames211 <riakjamesngong@gmail.com>
2024-10-03 16:30:21 +0300

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,9 @@
+function numbers(num_3, num_4){
+    var sum=num_3+num_4;
+    var sub=num_3-num_4;
+    var multi=num_3*num_4;
+    var div=num_3/num_4;
+    
+    console.log("sum:", sum,"sub:", sub,"multi:",multi,"div:", div);
+}
+numbers(45,60);
